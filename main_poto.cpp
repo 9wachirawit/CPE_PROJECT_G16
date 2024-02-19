45761a210bf4aaca812ffa41d55e5443f10bf565
John <karumanx003@gmail.com>
2024-02-19 18:36:33 +0700

    fix array show

--- main_poto.cpp
+++ main_poto.cpp
@@ -5,51 +5,60 @@
 #include <conio.h>
 using namespace std;
 
-const int width = 100, height = 24;
-int screen[width][width] = {};
+const int width = 100, height = 26;
+int screen[height][width] = {0};
 
-void drawBlock(int x, int y, int color) {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, color);
-    COORD pos = {x * 2, y};
-    SetConsoleCursorPosition(hConsole, pos);
-    cout << " ";
+
+//buildscene_inprogress pedophathora job
+void buildscene(int (&x)[height][width], int width, int height) {
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+            x[i][j] = (int)0;
+        }
+    }
 }
 
-int drawField(string a) {
-	
+//kaizer job
+/*void monsterspawn(int (&x)[height][width], int width, int height){
+	for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+    
+        }
+    }
+}*/
+
+//read_and_show_scene inprogress 
+int drawField(int (&screen)[height][width], int width, int height) {
     system("cls");
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            if (screen[x][y] == 0)
-                cout <<a;
-            else
-                drawBlock(x, y, screen[x][y]);
+                if(screen[y][x] == 0){
+                	cout << 0;
+				}
         }
         cout << endl;
     }
 }
 
+
+//keyboard system inprogress 
 int keyboard(char key,string & A){
 	if (key == 'A' || key == 'a') {
-                A = "+";
+                A = "<";
             }
         else if(key == 'D' || key == 'd'){
-        	A = "-";
+        	A = ">";
 				}
 			else if(key == 'W' || key == 'w'){
-			A = "o";
+			A = "^";
 					}
 				else if(key == 'S' || key == 's'){
-			A = "1";
+			A = "o";
 						}
 	}
             
             
-	
-
-
-/* inprogress
+//filesystem_inprogress kaizer job
 int findhistory(string tag){
 	ifstream tagfinder;
 	tagfinder.open("save\\saveid.txt"); 
@@ -65,23 +74,22 @@ int findhistory(string tag){
 	}
 	return -1;
 }
-*/
+
 
 
 int main() {
+	buildscene(screen, width, height);
 	ios_base::sync_with_stdio(false);
-	string a = ".";
 	int j = 0;
 	while (j == 0) { 
-        drawField(a);
-        Sleep(50); // Delay 500 milliseconds
-    	if (_kbhit()) {
+       drawField(screen, width, height);
+      
+        Sleep(50); // Delay
+    	/* if (_kbhit()) {
             char key = _getch();
             keyboard(key,a);
-    					}
+    					}*/
     
-
-    }
-
     return 0;
+}
 }
