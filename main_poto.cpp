9b892c484fefbf0141345719ba48d317a7d814c5
John <karumanx003@gmail.com>
2024-02-19 22:42:43 +0700

    fix loop

--- main_poto.cpp
+++ main_poto.cpp
@@ -6,14 +6,18 @@
 using namespace std;
 
 const int width = 100, height = 26;
-int screen[height][width] = {0};
+int screen[height][width] = {};
+string A = "o";
 
-
-//buildscene_inprogress pedophathora job
+//Zaoshang hao zhongguó xiànzài wo you bingqílín wo hen xihuan bingqílín
 void buildscene(int (&x)[height][width], int width, int height) {
     for(int i = 0; i < height; i++) {
         for(int j = 0; j < width; j++) {
-            x[i][j] = (int)0;
+            if (((i==1) and (j >= 25) and (j <= 74) and (j != 37) and (j != 38) and (j != 51) and (j != 52)) or ((i==22) and (j >= 25) and (j <= 74) and (j != 37) and (j != 38) and (j != 51) and (j != 52)) or ((i==24) and (j >= 25) and (j <= 74)) or ((j==25) and (i >= 1) and (i <= 22)) or ((j==74) and (i >= 1) and (i <= 22)) or ((j==36) and (i >= 1) and (i <= 22)) or ((j==39) and (i >= 1) and (i <= 22)) or ((j==50) and (i >= 1) and (i <= 22)) or ((j==53) and (i >= 1) and (i <= 22))) {
+                x[i][j] = 1;
+            } else {
+                x[i][j] = 0;
+            }
         }
     }
 }
@@ -29,11 +33,14 @@ void buildscene(int (&x)[height][width], int width, int height) {
 
 //read_and_show_scene inprogress 
 int drawField(int (&screen)[height][width], int width, int height) {
+	buildscene(screen, width, height);
     system("cls");
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
                 if(screen[y][x] == 0){
-                	cout << 0;
+                	cout << " ";
+				} else {
+                	cout << A;
 				}
         }
         cout << endl;
@@ -42,7 +49,7 @@ int drawField(int (&screen)[height][width], int width, int height) {
 
 
 //keyboard system inprogress 
-int keyboard(char key,string & A){
+int keyboard(char key,string &A){
 	if (key == 'A' || key == 'a') {
                 A = "<";
             }
@@ -78,18 +85,14 @@ int findhistory(string tag){
 
 
 int main() {
-	buildscene(screen, width, height);
-	ios_base::sync_with_stdio(false);
-	int j = 0;
-	while (j == 0) { 
-       drawField(screen, width, height);
-      
-        Sleep(50); // Delay
-    	/* if (_kbhit()) {
+    ios_base::sync_with_stdio(false);
+    while (true) {
+        drawField(screen, width, height);
+        Sleep(50);
+        if (_kbhit()) {
             char key = _getch();
-            keyboard(key,a);
-    					}*/
-    
+            keyboard(key,A);
+    					}
+    }
     return 0;
-}
 }
